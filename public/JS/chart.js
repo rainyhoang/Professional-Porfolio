24209cbf9deccf9dbee813b2c2593feef0164e4c
Le Vu Hoang <rainyhoang@gmail.com>
2018-01-10 22:50:30 -0500

    add-on

---
+++ public/JS/chart.js
@@ -0,0 +1,63 @@
+
+// Technical Skill
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ["Javascript", "jQuery", "React", "HTML", "CSS"],
+        datasets: [{
+            label: 'Level of skill',
+            data: [70, 75, 66, 85, 70],
+            backgroundColor: [
+                'rgba(255, 0, 0, 1)',
+                'rgba(255, 0, 0, 1)',
+                'rgba(255, 0, 0, 1)',
+                'rgba(255, 0, 0, 1)',
+                'rgba(255, 0, 0, 1)',
+            ],
+            
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+
+                    autoSkip: false,
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+//Marketing skill
+var ctx = document.getElementById("myChart2");
+var myChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: {
+        labels: ["Planning", "Executing", "Presentation", "Multitasking", "Report"],
+        datasets: [{
+            label: 'Level of skill',
+            data: [70, 85, 65, 90, 80],
+            backgroundColor: [
+                'rgba(0, 0, 255, 1)',
+                'rgba(0, 0, 255, 1)',
+                'rgba(0, 0, 255, 1)',
+                'rgba(0, 0, 255, 1)',
+                'rgba(0, 0, 255, 1)',
+            ],
+            
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    autoSkip: false,
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
